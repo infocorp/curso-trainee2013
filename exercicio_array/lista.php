a93bcd8c6e4632fe59abaf732d01fe126f04d938
hackmbh <marcelon.hack@gmail.com>
2013-07-27 17:32:47 -0400

    Adiciona enunciado de exercicio

--- exercicio_array/lista.php
+++ exercicio_array/lista.php
@@ -1,5 +1,8 @@
 <?php
-
+// Exercicio: recuperar string da textarea
+// do formulario e colocar os itens separados
+// por virgula dentro de um array.
+// O codigo abaixo mostra o caminho
 $arr = explode(',', 'item, item, item');
 echo '<pre>';
 print_r($arr);
